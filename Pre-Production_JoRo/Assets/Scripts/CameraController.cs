2f303349af665506ca69e2de94fe703158a6f901
joannashen1 <55471763+joannashen1@users.noreply.github.com>
2019-09-19 09:41:07 -0700

    reimported standard assets.
    Reimported Standard Assets.

---
+++ Pre-Production_JoRo/Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+
+    float distance;
+    Vector3 playerPrevPos, playerMoveDir;
+
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+
+        //distance = offset.magnitude;
+        //playerPrevPos = player.transform.position;
+    }
+
+    void LateUpdate()
+    {//camera update code should be under LateUpdate to avoid jittering
+
+        transform.position = player.transform.position + offset;
+
+        //playerMoveDir = player.transform.position - playerPrevPos; //how much player moved since last frame
+        //if (playerMoveDir != Vector3.zero)//!= means not equal to
+        //{
+           // playerMoveDir.Normalize(); //normalize" normalizes the vector (with mag) into 1 (direction)
+            //transform.position = player.transform.position - playerMoveDir * distance;
+            //Vector3 Move = transform.position;
+           // transform.position = new Vector3(Move.x, 2, Move.z); //taking transform.position apart using Move
+
+            //transform.Translate(0, 3, 0);
+
+            //transform.LookAt(player.transform.position);
+
+           // playerPrevPos = player.transform.position;
+        //}
+
+        //transform.position = player.transform.position + offset;
+    }
+}
---
+++ Pre-Production_JoRo/Assets/Scripts/Game_Options.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game_Options : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space pressed");
+            SceneManager.LoadScene(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("R pressed");
+            SceneManager.LoadScene(0);
+        }
+
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    SceneManager.LoadScene(1);
+        //}
+    }
+}
